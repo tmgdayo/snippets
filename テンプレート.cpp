dbd8de371de540249a2079507fee90496aa467c6
tmgdayo <tmgisland@gmail.com>
2021-05-22 16:28:02 +0900

    短縮化

--- テンプレート.cpp
+++ テンプレート.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<ll> vl;
-typedef vector<vl> vvl;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
 #define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
 #define FOR(i, a, b) for (int i = (a), i##_len = (b); i < i##_len; i++)
 #define ALL(x) (x).begin(), (x).end()  // sortなどの引数を省略したい
@@ -27,25 +27,19 @@ template <class Head, class... Tail> void PRINT(Head&& head, Tail&&... tail) {
     std::cout << head << ' ';
     PRINT(std::forward<Tail>(tail)...);
 }
-template <class T> auto MAX(T& seq) {
-    return *max_element(seq.begin(), seq.end());
-}
-template <class T> auto MIN(T& seq) {
-    return *min_element(seq.begin(), seq.end());
-}
+template <class T> auto MAX(T& seq) { return *max_element(ALL(seq)); }
+template <class T> auto MIN(T& seq) { return *min_element(ALL(seq)); }
 template <class T> auto SUM(T& seq) {
     T temp{0};
     auto& temp2 = temp[0];
     return accumulate(seq.begin(), seq.end(), temp2);
 }
-template <class T> void REV(vector<T>& seq) { reverse(seq.begin(), seq.end()); }
-template <class T> void SORT(T& seq) { sort(seq.begin(), seq.end()); }
+template <class T> void REV(vector<T>& seq) { reverse(ALL(seq)); }
+template <class T> void SORT(T& seq) { sort(ALL(seq)); }
 template <class T, class S> void SORT(T& seq, S& sort_order) {
-    sort(seq.begin(), seq.end(), sort_order);
+    sort(ALL(seq), sort_order);
 }
-template <class T> void SORTR(vector<T>& seq) {
-    sort(seq.begin(), seq.end(), greater<T>());
-}
+template <class T> void SORTR(vector<T>& seq) { sort(ALL(seq), greater<T>()); }
 template <class T> int pow(int n_0, T k, int mod) {
     if (n_0 >= mod) n_0 %= mod;
     if (n_0 < 0) n_0 += mod;
@@ -70,8 +64,8 @@ template <typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
 }
 void yn(bool flag) {
     if (flag) {
-        cout << "YES" << '\n';
+        PRINT("YES");
     } else {
-        cout << "NO" << '\n';
+        PRINT("NO");
     }
 }
